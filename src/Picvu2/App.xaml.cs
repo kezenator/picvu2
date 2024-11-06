617da4ed49ea7a8332e757db66b0f0eaca932ea7
kezenator <github@kezenator.com>
2024-11-06 20:09:13 +1000

    Initial commit of basic WPF application

---
+++ src/Picvu2/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.Design;
+using System.Windows;
+
+namespace Picvu2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IServiceProvider? ServiceProvider { get; private set; }
+        public IConfiguration? Configuration { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Configuration = new ConfigurationBuilder()
+                .Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection serviceContainer)
+        {
+            serviceContainer.AddTransient<MainWindow, MainWindow>();
+        }
+    }
+
+}
